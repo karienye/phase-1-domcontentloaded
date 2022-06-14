0bb4b383f4616206040e22de476ee32f2846dccd
leah karienye <karienyeleah@gmail.com>
2022-06-14 11:06:27 +0300

    commit tree bdb8802d8aa03287f7a45ee1737ca83bd3995b3f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+/*document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });*/
+  
+ // console.log ('This is really cool!')
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    updateDom()
+  });
+  document.getElementById('text').innerHTML = "This is really cool" ;
+  /*console.log(
+    "This is really cool!' to equal 'JavaScript is so cool. It lets me add text to my page programmatically."
+  );*/
