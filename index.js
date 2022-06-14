3f913ac1c7ab68eb3a7d0ed3cf8c5760c71db7c2
leah karienye <karienyeleah@gmail.com>
2022-06-14 11:16:37 +0300

    content loaded

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+/*document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });*/
+  
+ // console.log ('This is really cool!')
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    updateDom()
+  });
+  function updateDom () {
+  document.getElementById('text') .innerHTML = "This is really cool!" ;
+  }
+ 
